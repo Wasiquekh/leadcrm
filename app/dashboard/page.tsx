a1d53fca5d7e17ad6a5876864a873281f50370d0
wasique@dynsimulation.com <wasique@dynsimulation.com>
2025-10-04 16:15:22 -0400

    minor

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -206,7 +206,7 @@ useEffect(() => {
           {userRole === "Agent" && (
             <div className=" flex justify-end items-center gap-4">
               <div
-                className=" flex justify-center gap-2 py-3 px-6 rounded-[4px] border border-[#E7E7E7] cursor-pointer bg-primary-600 items-center hover:bg-primary-700 active:bg-primary-800 group"
+                className=" flex justify-center gap-2 py-3 px-6 rounded-[12px] border border-[#E7E7E7] cursor-pointer bg-primary-600 items-center hover:bg-primary-700 active:bg-primary-800 group"
                 onClick={() => createLeads()}
               >
                 <FiPlusCircle className=" w-5 h-5 text-white group-hover:text-white" />
@@ -216,7 +216,7 @@ useEffect(() => {
               </div>
 
               <div
-                className=" flex justify-center  gap-2 py-3 px-6 rounded-[4px] border border-[#E7E7E7] cursor-pointer bg-primary-600 items-center hover:bg-primary-700 active:bg-primary-800 group"
+                className=" flex justify-center  gap-2 py-3 px-6 rounded-[12px] border border-[#E7E7E7] cursor-pointer bg-primary-600 items-center hover:bg-primary-700 active:bg-primary-800 group"
                 onClick={() => filterLeads()}
               >
                 <FaSearchPlus className=" w-5 h-5 text-white group-hover:text-white" />
